34cc6aab6a5c6fc4e7ce2f3835d8f5c7a9a98916
niels <nielsrominger@gmail.com>
2025-06-30 23:16:45 +0200

    m

---
+++ flows/testapp.py
@@ -0,0 +1,13 @@
+from onecode import slider, file_output, Logger
+
+
+def run():
+    n = slider("Table de multiplication jusqu’à", value=5, min=1, max=100)
+
+    output_file = file_output("Fichier résultat", "multiplication.txt")
+
+    with open(output_file, "w") as f:
+        for i in range(1, n + 1):
+            line = f"{i} x {i} = {i * i}"
+            f.write(line + "\n")
+            Logger.info(line)
